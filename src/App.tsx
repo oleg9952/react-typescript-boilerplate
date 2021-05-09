df323dca806997afe7e660d58699d1002c9b35fe
oleg9952 <oleg.parhomenko2599@gmail.com>
2021-05-09 14:02:47 +0300

    ARCH: Configure webpack env configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Logo from '../public/assets/react-logo.jpeg';
+import Logo from '/public/assets/react-logo.jpeg';
 
 const App = () => {
     return (
         <>
             <img src={Logo} alt="react logo"/>
-            <h1>Hello World</h1>
+            <h1>React | TypeScript</h1>
         </>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './theme/index.scss';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.getElementById('root'));
