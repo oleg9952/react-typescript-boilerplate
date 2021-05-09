a110d5e3f997fc5afc8285ab96003ce4ac4c6308
oleg9952 <oleg.parhomenko2599@gmail.com>
2021-05-09 14:21:09 +0300

    ARCH: Integrate react-refresh for dev env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import Logo from '/public/assets/react-logo.jpeg';
 
 const App = () => {
+    const [count, setCount] = useState(0);
+
     return (
         <>
             <img src={Logo} alt="react logo"/>
-            <h1>React | TypeScript</h1>
+            <h1>React & TypeScript</h1>
+            <button onClick={() => setCount(count + 1)}>{count}</button>
         </>
     )
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,12 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     devtool: 'cheap-module-source-map',
+    devServer: {
+        hot: true
+    },
+    plugins: [
+        new ReactRefreshWebpackPlugin()
+    ]
 }
