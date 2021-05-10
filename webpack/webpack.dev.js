e0313ccd5506dfabb47a6c17fc58cf465dacefb3
Oleg Parhomenko <48036701+oleg9952@users.noreply.github.com>
2021-05-10 14:35:52 +0300

    Setup boilerplate (#1)

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,10 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        hot: true,
+    },
+    plugins: [new ReactRefreshWebpackPlugin()],
+};
